98c3a17e04b3d27459dd3aa7ee0f0809b2852228
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2023-03-13 16:07:34 +0500

    functionality leftt

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,15 @@
 $(document).ready(() => {
     let drag_id;
     $(function (drag_id) {
-       $("#box1").draggable();
-      });      
+       $("#box1 ,#box2,#box3 ,#box4").draggable();
+      });    
+         //Add  boxes;
+  $("#addBoxes").click(function () {
+    var newBox = $('<div class="box" id="box5" data-bs-toggle="modal" data-bs-target="#exampleModal"><div class="item" id="item4"><button id="b5" onclick="return checkValue("b5")"></button></div></div>') 
+    $('.box-left').append(newBox);
+});
     })
+   
 //   <!--  Modal-->
 $(document).ready(function () {
     $(".box").click(function () {
@@ -11,18 +17,54 @@ $(document).ready(function () {
     });
   });
  
-  //Add  boxes;
-  $("#addBoxes").click(function () {
-    $(".box-left").append
-    ('<div class="box" id="box5" data-bs-toggle="modal" data-bs-target="#exampleModal"><div class="item" id="item4"><button id="b5" onclick="return checkValue("b5")"></button></div></div>')
-    alert('add button clicked');
-});
-
 
 let id;
+let btn1;
+let btnId;
+
+function getBtnId(clicked_id)
+{
+  var id = clicked_id;
+    console.log(id + ' getBtnFunction');
+    var btn1 = document.getElementById(id);
+    console.log(btn1);
+    var btnId = $(btn1).attr("id");
+    console.log(btnId + ' getBtnFunction');  
+
+  function checkValue() {
+     id = btnId;
+     console.log(id + ' checkValueFunction' );
+   var inputvalue = document.getElementById('modal1');
+   console.log(inputvalue.value + ' inputValue');
+   var buttonsubmit = document.getElementById('submit');
+   var value = inputvalue.value;
+   console.log(value);
+   id.innerText = value; 
+
+};
+checkValue();
+  //Change the color:
+function changeColor() {
+  
+  var button1 = btn1;
+  console.log(button1);
+  var bgColor = document.getElementById("GFG_Color")
+  button1.style.backgroundColor = bgColor.value;
+  console.log(bgColor.value);
+  // if(bgColor.value === "rgb(255, 255, 255)"){
+  //     button1.style.color = 'white';
+  // }
+};
+changeColor();
 //TEXT UPDATE: 
-var btn1 = document.getElementById(id);
-console.log(btn1);
+// $(".box").click(function(clicked_id){
+//   var id = clicked_id;
+//   console.log(id);
+//   var btn1 = $(this.id).attr("id");
+//   console.log(btn1);
+// });
+};
+
 // Get the modal
 var modal = document.getElementById('exampleModal');
 // Get the <span> element that closes the modal
@@ -32,27 +74,7 @@ closeBtn.onclick = function() {
     modal.style.display = "none";
 }
 
-//Change the color:
-function changeColor() {
-    var button1 = btn1;
-    var bgColor = document.getElementById("GFG_Color")
-    button1.style.backgroundColor = bgColor.value;
-    if(bgColor.value === "rgb(255, 255, 255)"){
-        button1.style.color = 'white';
-    }
-  };
 
-// Arithematic Operator Control
-function checkValue(btnid) { 
-    id = btnid;
-    console.log(id);
-  var inputvalue = document.getElementById('modal1');
-  var buttonsubmit = document.getElementById('submit');
-  var value = inputvalue.value;
-  console.log(value);
-  id.innerText = value; 
-  return false;
-};
 
 $('#submit').click(function() {
     $('#exampleModal').modal('hide');
